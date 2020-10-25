44738ea5db552a80486e363fda2731177b3e2f73
Liad Oz <liadozil@gmail.com>
2020-10-25 08:12:38 +0200

    added shorcuts and literate progrmming support

--- .zsh_defaults.sh
+++ .zsh_defaults.sh
@@ -16,3 +16,6 @@ echo " ''|...|'  ||......|  ''|...|'  .|.   '|  ||......|  ''|...|'  .|.   '|   
 export LIBGL_ALWAYS_INDIRECT=1
 alias emacs="setsid emacs"
 alias python=python3
+alias pt="python -m unittest"
+export FLASK_ENV=development
+alias rser=flask run --host=0.0.0.0
