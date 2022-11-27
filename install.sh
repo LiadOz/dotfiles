7103239ef3a2081b660144fd098205df2c968080
Liad Oz <liadozil@gmail.com>
2022-11-27 20:46:28 +0200

    added nvim directory, chmod binaries

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git submodule update --init --recursive
 
+chmod u+x binaries/.local/bin/*
 stow binaries gdb nvim tmux zsh
 SOURCE_STR="source $HOME/.zsh_global.sh"
 ZSH_FILE="$HOME/.zshrc"
