e1bf82266641a8b30851eef650752384acac9774
Liad Oz <liadozil@gmail.com>
2025-04-07 13:41:19 +0300

    feat: add colordiff config, nvim updates

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git submodule update --remote --merge
 git submodule foreach --recursive git checkout master
 
 chmod u+x binaries/.local/bin/*
-stow binaries gdb nvim tmux zsh
+stow binaries gdb nvim tmux zsh colordiff
 SOURCE_STR="source $HOME/.zsh_global.sh"
 ZSH_FILE="$HOME/.zshrc"
 grep -q "$SOURCE_STR" "$ZSH_FILE" || echo "$SOURCE_STR" >> "$ZSH_FILE"
