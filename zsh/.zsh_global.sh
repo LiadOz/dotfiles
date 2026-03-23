054b71f60282bf3176d2364792c7278ec95348b5
Liad Oz <loz@nvidia.com>
2026-03-23 12:35:50 +0200

    fixes for taskwarrior

--- zsh/.zsh_global.sh
+++ zsh/.zsh_global.sh
@@ -19,3 +19,39 @@ tickle () {
 }
 alias tick=tickle
 alias think='tickle +1d'
+
+taskb() {
+    if [ -z "$1" ]; then
+        echo "Usage: taskb <task-id>"
+        return 1
+    fi
+
+    task_id="$1"
+    branch=$(git branch --show-current 2>/dev/null)
+    if [ -z "$branch" ]; then
+        echo "Not inside a Git repo."
+        return 1
+    fi
+
+    task "$task_id" modify branch:$branch
+}
+
+wtom () {
+    task "$1" modify wait:tomorrow
+}
+
+wsun () {
+    task "$1" modify wait:$(date -d 'next sunday' +%F)
+}
+
+wday () {
+    task "$1" modify wait:"+${2}d"
+}
+
+rnext () {
+    task "$1" modify -next +review
+}
+
+notify () {
+    task "$1" modify -review +notify
+}
