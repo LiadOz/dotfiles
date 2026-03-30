2502c9e642a71b683e7eb853c0161dc7d4199d80
LiadOz <liadozil@gmail.com>
2026-03-30 08:45:54 +0300

    remove -R from stow

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ git submodule update --remote --merge
 git submodule foreach --recursive git checkout master
 
 chmod u+x binaries/.local/bin/*
-stow -R binaries gdb nvim tmux zsh colordiff task
+stow binaries gdb nvim tmux zsh colordiff task
 SOURCE_STR="source $HOME/.zsh_global.sh"
 ZSH_FILE="$HOME/.zshrc"
 grep -q "$SOURCE_STR" "$ZSH_FILE" || echo "$SOURCE_STR" >> "$ZSH_FILE"
